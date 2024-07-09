b876b290bd306ee40c968f8f2796d7395c8e3a31
tarrun <tarunloharwnsp2017@gmail.com>
2024-07-09 08:54:36 +0530

    new changes

--- linkedin/demo/SignupVC.swift
+++ linkedin/demo/SignupVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class SignupVC: UIViewController {
 
@@ -20,20 +22,63 @@ class SignupVC: UIViewController {
     @IBOutlet weak var passwordTF: UITextField!
     
     @IBAction func actLogin(_ sender: UIButton) {
-        let email = self.emailTF.text!
-        let pass = self.passwordTF.text!
-        guard validation(email: email) else {
-            print("Enter Correct email")
-            return
-        }
-        
-        delegate?.sendDataToPreviousVC(email: email, password: pass)
-        navigationController?.popViewController(animated: true)
+//        let email = self.emailTF.text!
+//        let pass = self.passwordTF.text!
+//        guard validation(email: email) else {
+//            print("Enter Correct email")
+//            return
+//        }
+//        
+//        delegate?.sendDataToPreviousVC(email: email, password: pass)
+//        navigationController?.popViewController(animated: true)
+        signUpWithFirebase()
 
     }
     
     @IBAction func actBack(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    func signUpWithFirebase(){
+        guard let email = self.emailTF.text, !email.isEmpty,
+              let password = self.passwordTF.text, !password.isEmpty else{
+            return
+        }
+        Auth.auth().createUser(withEmail: email, password: password){(authResult, error) in
+            if let nsError = error as NSError?{
+                if let errorCode = AuthErrorCode.Code(rawValue: nsError.code){
+                    switch errorCode{
+                    case .invalidEmail:
+                        print("Invalid Email")
+                    case .emailAlreadyInUse:
+                        print("email is already in use")
+                    case .weakPassword:
+                        print("week password")
+                    default:
+                        print("out of range")
+                    }
+                    
+                }else{
+                    print("Login successful")
+                }
+            }
+            
+            let uuid = authResult?.user.uid
+            print(uuid!)
+            let db = Firestore.firestore()
+            
+            db.collection("user").document(uuid!).setData([
+                "name":"tarun lohar",
+                "mobile":"8000970578",
+                "city": "kankroli"
+            ]){error in
+                if let error = error{
+                    print("Error saving user data: \(error.localizedDescription)")
+                }else{
+                    print("User details save successfully!")
+                }
+            }
+        }
     }
     
 }
